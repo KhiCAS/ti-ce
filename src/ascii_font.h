e975ec3ad713c65b5a3a75e59311da2a6c85c316
zerico <71151164+ZERICO2005@users.noreply.github.com>
2026-01-25 18:25:54 -0700

    moved font data

---
+++ src/ascii_font.h
@@ -0,0 +1,19 @@
+#ifndef ASCII_FONT_H
+#define ASCII_FONT_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern const unsigned char VGA_Ascii_5x8[];
+extern const unsigned char orp_Ascii_6x12[];
+extern const unsigned char VGA_Ascii_8x16[];
+extern const unsigned char VGA_Ascii_7x14[];
+
+// extern const unsigned char VGA_Ascii_6x12[];
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ASCII_FONT_H */
